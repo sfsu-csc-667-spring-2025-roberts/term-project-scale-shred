edaacf2e64f9bb129a09f99a0a47cb1c94dfd8a6
Ulices Gonzalez <sergioulicesgonzalezguerrero@gmail.com>
2025-03-20 18:39:22 -0700

    added preliminary styling assets and change the icon. updated info present by root just so its a bit more personalized.

--- src/server/routes/root.ts
+++ src/server/routes/root.ts
@@ -3,8 +3,8 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (request, response) => {
-  const title = "Uno Website";
-  const name = "Scale Shred";
+  const title = "Scale Shred UNO";
+  const name = "Team 12";
 
   // short for: const thing = { title: title };
   response.render("root", { title, name });
